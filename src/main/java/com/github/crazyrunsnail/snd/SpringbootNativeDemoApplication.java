b34ccc73b90bc9116432d896e6752bb62388af5e
crazyrunsnail <fjyuan1991@163.com>
2025-12-10 00:55:18 +0800

    Add configuration and properties

--- src/main/java/com/github/crazyrunsnail/snd/SpringbootNativeDemoApplication.java
+++ src/main/java/com/github/crazyrunsnail/snd/SpringbootNativeDemoApplication.java
@@ -1,11 +1,16 @@
 package com.github.crazyrunsnail.snd;
 
+import com.github.crazyrunsnail.snd.model.User;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
+@MapperScan(basePackages = "com.github.crazyrunsnail.snd.mapper", sqlSessionFactoryRef = "sqlSessionFactory", sqlSessionTemplateRef = "sqlSessionTemplate")
 public class SpringbootNativeDemoApplication {
 
 	public static void main(String[] args) {
